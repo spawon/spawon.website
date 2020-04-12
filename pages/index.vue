64859354802e26e9435550ad00befa2cd1beb77f
Vyacheslav Maslov <spawon@nxt.ru>
2020-04-12 19:35:47 +0300

    test commit

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,10 @@
         Nuxt.js + Vuesax
       </h1>
       <h2 class="subtitle">
-        <a href="https://vuesax.com/">Vuesax</a> is a framework of ui components for <a href="https://vuejs.org/">Vuejs</a>, It was created to make new interfaces that have a new trend and are visually beautiful
+        <a href="https://vuesax.com/">Vuesax</a> is a framework of ui components for <a
+          href="https://vuejs.org/"
+        >Vuejs</a>, It was created to make new interfaces that have a new trend and are
+        visually beautiful
       </h2>
       <div class="links">
         <h3 class="h3">
@@ -74,61 +77,63 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 55px;
-  color: #35495e;
-  letter-spacing: 1px;
-  text-transform: capitalize;
-  margin: 25px 0px
-}
+    display: block;
+    font-weight: 300;
+    font-size: 55px;
+    color: #35495e;
+    letter-spacing: 1px;
+    text-transform: capitalize;
+    margin: 25px 0px
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 1.1rem;
-  color: #526488;
-  word-spacing: 2px;
-  padding-bottom: 15px;
-  max-width: 600px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 1.1rem;
+    color: #526488;
+    word-spacing: 2px;
+    padding-bottom: 15px;
+    max-width: 600px;
+  }
 
-.subtitle a {
-  font-weight: 500;
-  color: inherit
-}
+  .subtitle a {
+    font-weight: 500;
+    color: inherit
+  }
 
-.links {
-  padding-top: 15px;
-  margin-bottom: 20px
-}
+  .links {
+    padding-top: 15px;
+    margin-bottom: 20px
+  }
 
-.content-logos {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 500px;
-}
-.plus {
-  font-size: 2.5rem;
-  margin: 15px;
-  color: #35495e
-}
-.h3 {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  .content-logos {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 500px;
+  }
+
+  .plus {
+    font-size: 2.5rem;
+    margin: 15px;
+    color: #35495e
+  }
+
+  .h3 {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-weight: 400;
-  margin: 10px;
-}
+    font-weight: 400;
+    margin: 10px;
+  }
 </style>
