39e98cae0b9b2ae9366cd7fca1c322d159f7d40f
Vyacheslav Maslov <spawon@nxt.ru>
2020-04-12 23:57:45 +0300

    update pm

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [{
+    name: 'dev',
+    script: 'server/index.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'node',
+      host: '127.0.0.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
